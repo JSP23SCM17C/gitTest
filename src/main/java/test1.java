d0aa696e1f50d4d3dead085178742ff98bf6984a
Jinke <jinkchen3@gmail.com>
2024-04-04 08:36:45 -0700

    222222

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,6 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("777");
-        System.out.println("66666");
+        System.out.println("666662222222");
     }
 }
