c984d92ecba8d455f55e6c7616bb727fdc7219cc
Jinke Chen <129140494+JSP23SCM17C@users.noreply.github.com>
2024-04-04 11:06:39 -0700

    Update test1.java

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -6,7 +6,7 @@ public class test1 {
         System.out.println("555555");
         System.out.println("44444444");
         System.out.println("33333333");
-         System.out.println("222 from github");
+         System.out.println("222 from github 222");
         
     }
 }
